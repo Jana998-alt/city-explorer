5e6ecf97ff08d9ded73c2df240ce877522ef3f9d
Jana Abdelkader <janaosama1300@gmail.com>
2021-07-05 00:02:57 +0300

    getting lon and lat

--- src/App.js
+++ src/App.js
@@ -1,10 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import React from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { construct } from 'harmony-reflect';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+// import reactDom from 'react-dom';
 
-function App() {
-  return (
+class App extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      cityName : '',
+      lon: 0,
+      lat: 0, 
+    }
+  }
+
+  getLocation = async (event) => {
+
+   event.preventDefault();
+
+    // let cityName = 
+    let url = `https://eu1.locationiq.com/v1/search.php?key=pk.3130cae6529e5d2c225c5f40822dca62&q=${event.target.cityName.value}&format=json`;
+
+  
+    let theData = await axios.get(url);
+    
+    this.setState ({
+      cityName : event.target.cityName.value,
+      lon : theData.data[0].lon,
+      lat : theData.data[0].lat
+    })
+    console.log(theData.data[0].lat);
+  }
+  
+  
+  render() { 
+
+    return(
     <div className="App">
-      <header className="App-header">
+      <form onSubmit={this.getLocation}>
+        <label>Input city Name:</label>
+        <input type="text" name="cityName"/>
+        <br/>
+        <button type="submit" >Explore!</button>
+      </form>
+
+      <Card style={{ width: '18rem'}}>
+      <Card.Header>{`${this.state.cityName}`}</Card.Header>
+      <ListGroup variant="flush">
+        <ListGroup.Item>{`longitude: ${this.state.lon}`}</ListGroup.Item>
+        <ListGroup.Item>{`latitude: ${this.state.lat}`}</ListGroup.Item>
+        
+      </ListGroup>
+    </Card>
+    </div>
+  )
+}
+}
+
+export default App;
+
+
+
+
+// https://jana-city-explorer.netlify.app/
+
+
+
+
+
+
+{/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,10 +85,5 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+        </a> */}
+      {/* </header> */}
