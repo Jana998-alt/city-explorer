bc98aa47c2f7278d960896cfb803a46a42e2e0d4
Jana Abdelkader <janaosama1300@gmail.com>
2021-07-05 09:00:40 +0300

    part two of lab06

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import './App.css';
+import './App.css';
 import React from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -14,7 +14,8 @@ class App extends React.Component {
     this.state = {
       cityName : '',
       lon: 0,
-      lat: 0, 
+      lat: 0,
+      map:'', 
     }
   }
 
@@ -25,15 +26,14 @@ class App extends React.Component {
     // let cityName = 
     let url = `https://eu1.locationiq.com/v1/search.php?key=pk.3130cae6529e5d2c225c5f40822dca62&q=${event.target.cityName.value}&format=json`;
 
-  
     let theData = await axios.get(url);
-    
+
     this.setState ({
       cityName : event.target.cityName.value,
       lon : theData.data[0].lon,
-      lat : theData.data[0].lat
+      lat : theData.data[0].lat,
     })
-    console.log(theData.data[0].lat);
+
   }
   
   
@@ -41,14 +41,20 @@ class App extends React.Component {
 
     return(
     <div className="App">
+      <header className="App-header">
+        <h1>City Explorer!</h1>
+      </header>
+      
       <form onSubmit={this.getLocation}>
-        <label>Input city Name:</label>
-        <input type="text" name="cityName"/>
+        <label className="formContent">Input city Name:</label>
         <br/>
-        <button type="submit" >Explore!</button>
+        <input className="formContent" type="text" name="cityName"/>
+        <br/>
+        <button className="formContent" type="submit" >Explore!</button>
       </form>
 
       <Card style={{ width: '18rem'}}>
+      <Card.Img variant="top" src={`https://maps.locationiq.com/v3/staticmap?key=pk.3130cae6529e5d2c225c5f40822dca62&center=${this.state.lat},${this.state.lon}&zoom=13`} alt={this.state.cityName}/>
       <Card.Header>{`${this.state.cityName}`}</Card.Header>
       <ListGroup variant="flush">
         <ListGroup.Item>{`longitude: ${this.state.lon}`}</ListGroup.Item>
@@ -66,24 +72,4 @@ export default App;
 
 
 
-// https://jana-city-explorer.netlify.app/
-
-
-
-
-
-
-{/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      {/* </header> */}
+// https://jana-city-explorer.netlify.app/ 
